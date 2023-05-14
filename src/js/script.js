c46be07b1e06aeb4b899b363240d82d9a0daa15f
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-05-14 22:48:06 +0200

    created discription popup functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
+'use strict';
+
 const discriptionBtn = document.querySelector('.discription-btn');
 const discriptionPopup = document.querySelector('.discription');
 const BurgerBtn = document.querySelector('.burger');
 
 function showDiscription() {
-	discriptionPopup.classList.toggle('burger-active')
+	// discriptionPopup.classList.toggle('burger-active')
+	if (discriptionPopup.style.display === 'none') {
+		discriptionPopup.style.display = 'flex';
+	} else {
+		discriptionPopup.style.display = 'none';
+	}
 }
 
-BurgerBtn.addEventListener('click', showDiscription)
+BurgerBtn.addEventListener('click', showDiscription);
 discriptionBtn.addEventListener('click', showDiscription);
