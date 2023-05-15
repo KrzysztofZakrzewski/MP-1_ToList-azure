f277e54d460c9e780e1a0d74345dbf30b3fd9c99
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-05-15 02:34:32 +0200

    add functionality to add buton and create all cell

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,16 @@
-'use strict';
+// 'use strict';
+
+// const { create } = require('browser-sync');
 
 const discriptionBtn = document.querySelector('.discription-btn');
 const discriptionPopup = document.querySelector('.discription');
 const BurgerBtn = document.querySelector('.burger');
+let newToDo;
+let thing;
+
+const addBtn = document.querySelector('.btn-add');
+let todoInput = document.querySelector('.todo-input');
+const toDoList = document.querySelector('.todo-list');
 
 function showDiscription() {
 	// discriptionPopup.classList.toggle('burger-active')
@@ -13,5 +21,52 @@ function showDiscription() {
 	}
 }
 
+function addNewTask() {
+	if (todoInput.value !== '') {
+		newToDo = document.createElement('div');
+		newToDo.classList.add('cell');
+
+		toDoList.append(newToDo);
+		createThinkParagraph();
+		createToolsArea();
+		// newTodo.append(toolsPanel);
+		// newToDo.append(thing)
+		console.log(newToDo);
+	}
+}
+
+function createToolsArea() {
+	const toolsPanel = document.createElement('div');
+	toolsPanel.classList.add('tools');
+	// toolsPanel.style.display = 'block'
+
+	const completeBtn = document.createElement('button');
+	completeBtn.classList.add('complete', 'tools-btn');
+	completeBtn.textContent = '✅';
+	completeBtn.style.margin = '0 5px 0 0px';
+
+	const editBtn = document.createElement('button');
+	editBtn.classList.add('edit', 'tools-btn');
+	editBtn.textContent = 'EDIT';
+	editBtn.style.margin = '0 5px 0 0px';
+
+	const deleteBtn = document.createElement('button');
+	deleteBtn.classList.add('delete', 'tools-btn');
+	deleteBtn.textContent = '❌';
+	// deleteBtn.style.margin = '0 5px 0 5px';
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn);
+	newToDo.append(toolsPanel);
+}
+
+function createThinkParagraph() {
+	thing = document.createElement('p');
+	thing.classList.add('thing');
+	thing.textContent = todoInput.value;
+	newToDo.append(thing);
+}
+
 BurgerBtn.addEventListener('click', showDiscription);
 discriptionBtn.addEventListener('click', showDiscription);
+
+addBtn.addEventListener('click', addNewTask);
